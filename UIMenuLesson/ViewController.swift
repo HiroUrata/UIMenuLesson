546cd957506336784223df9e003c27ee53751b53
HiroUrata <1712026@i.softbank.jp>
2021-10-10 18:00:16 +0900

    UIMenu ３種類追加

--- UIMenuLesson/ViewController.swift
+++ UIMenuLesson/ViewController.swift
@@ -12,42 +12,132 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let menuButton = {() -> UIButton in
+//displayInlineMenuButton
+        let displayInlineMenuButton = {() -> UIButton in
             
             let button = UIButton(frame: CGRect(x: view.frame.maxX / 4, y: view.frame.maxY / 10, width: view.frame.width / 2, height: view.frame.height / 20))
-            button.setTitle("MenuButton", for: .normal)
+            button.setTitle("displayInlineMenu", for: .normal)
             button.titleLabel?.textColor = .black
             button.backgroundColor = .systemGreen
-            
+            button.showsMenuAsPrimaryAction = true
             return button
         }()
-        view.addSubview(menuButton)
+        view.addSubview(displayInlineMenuButton)
         
-        menuButton.menu = UIMenu(title:"",children: [
-        
-                                    UIMenu(title: "", options: .displayInline, children: [
-                                    
-                                        UIAction(title:"menu01",handler: { _ in
-                                            print("menu01が押されました")
-                                        }),
-                                        UIAction(title:"menu02",handler: { _ in
-                                            print("menu02が押されました")
-                                        }),
-                                        UIAction(title:"menu03",handler: { _ in
-                                            print("menu03が押されました")
-                                        }),
-                                        UIAction(title:"menu04",handler: { _ in
-                                            print("menu04が押されました")
-                                        }),
-                                        UIAction(title:"menu05",handler: { _ in
-                                            print("menu05が押されました")
-                                        })
-                                   ])
+        displayInlineMenuButton.menu = UIMenu(title:"displayInlineMenu",children: [
+            
+            UIMenu(title: "", options: .displayInline, children: [
+                
+                UIAction(title:"displayInlineMenu01",handler: { _ in
+                    print("displayInlineMenu01が押されました")
+                }),
+                UIAction(title:"displayInlineMenu02",handler: { _ in
+                    print("displayInlineMenu02が押されました")
+                }),
+                UIAction(title:"displayInlineMenu03",handler: { _ in
+                    print("displayInlineMenu03が押されました")
+                }),
+                UIAction(title:"displayInlineMenu04",handler: { _ in
+                    print("displayInlineMenu04が押されました")
+                }),
+                UIAction(title:"displayInlineMenu05",handler: { _ in
+                    print("displayInlineMenu05が押されました")
+                })
+            ])
         ])
         
-        menuButton.showsMenuAsPrimaryAction = true
+        
+//destructiveMenuButton
+        let destructiveMenuButton = {() -> UIButton in
+            
+            let button = UIButton(frame: CGRect(x: view.frame.maxX / 4, y: view.frame.maxY / 5, width: view.frame.width / 2, height: view.frame.height / 20))
+            button.setTitle("destructiveMenu", for: .normal)
+            button.titleLabel?.textColor = .black
+            button.backgroundColor = .systemBlue
+            button.showsMenuAsPrimaryAction = true
+            return button
+        }()
+        view.addSubview(destructiveMenuButton)
+        
+        destructiveMenuButton.menu = UIMenu(title:"destructiveMenu",children: [
+            
+            UIMenu(title: "destructiveMenu", options: .destructive, children: [
+                
+                UIAction(title:"destructiveMenu01",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu02",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu03",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu04",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu05",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                })
+            ])
+        ])
+        
+        
+//displayInlineMenu + destructiveMenu
+        let displayInlineAnddestructiveMenuButton = {() -> UIButton in
+            
+            let button = UIButton(frame: CGRect(x: view.frame.maxX / 4, y: view.frame.maxY / 3.3, width: view.frame.width / 2, height: view.frame.height / 20))
+            button.setTitle("displayInlineAnddestructiveMenu", for: .normal)
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+            button.titleLabel?.textColor = .black
+            button.backgroundColor = .systemRed
+            button.showsMenuAsPrimaryAction = true
+            return button
+        }()
+        view.addSubview(displayInlineAnddestructiveMenuButton)
+        
+        displayInlineAnddestructiveMenuButton.menu = UIMenu(title:"displayInlineMenu",children: [
+            
+            UIMenu(title: "", options: .displayInline, children: [
+                
+                UIAction(title:"displayInlineMenu01",handler: { _ in
+                    print("displayInlineMenu01が押されました")
+                }),
+                UIAction(title:"displayInlineMenu02",handler: { _ in
+                    print("displayInlineMenu02が押されました")
+                }),
+                UIAction(title:"displayInlineMenu03",handler: { _ in
+                    print("displayInlineMenu03が押されました")
+                }),
+                UIAction(title:"displayInlineMenu04",handler: { _ in
+                    print("displayInlineMenu04が押されました")
+                }),
+                UIAction(title:"displayInlineMenu05",handler: { _ in
+                    print("displayInlineMenu05が押されました")
+                })
+            ]),
+            UIMenu(title: "destructiveMenu", options: .destructive, children: [
+                
+                UIAction(title:"destructiveMenu01",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu02",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu03",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu04",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                }),
+                UIAction(title:"destructiveMenu05",handler: { _ in
+                    print("destructiveMenu01が押されました")
+                })
+            ])
+            
+        ])
+        
     }
-
-
+    
+    
 }
 
